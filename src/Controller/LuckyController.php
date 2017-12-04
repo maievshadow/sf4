bbe09e51d6294dda8bca930b91ef9626a803dc92
maievshadow <fz.uuuu@gmail.com>
2017-12-04 15:43:54 +0800

    first view page

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -6,16 +6,34 @@
  * Time: 下午2:20
  */
 namespace App\Controller;
-use Symfony\Component\HttpFoundation\Response;
 
-class LuckyController
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class LuckyController extends Controller
 {
-    public function number()
+    /**
+     * @Route("/lucky/number")
+     */
+    public function number():Response
     {
-        $number = mt_rand(0, 100);
+        $number = mt_rand(0, 20);
 
         return new Response(
             '<html><body>Lucky number: '.$number.'</body></html>'
         );
+    }
+
+    /**
+     * @Route("/lucky/number2")
+     */
+    public function number2():Response
+    {
+        $number = mt_rand(0, 20);
+
+        return $this->render('lucky/number.html.twig', array(
+            'number' => $number,
+        ));
     }
 }
